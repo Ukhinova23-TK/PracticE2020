e8ce45d123d34578b24fb9d8f48142cef6c4fd5a
Тома Ухинова <tatulya2001@gmail.com>
2020-05-08 18:21:47 +0300

    Create

--- Z08_1/Z08_1/Program.cs
+++ Z08_1/Z08_1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Z08_1
 {
@@ -10,6 +7,38 @@ namespace Z08_1
     {
         static void Main(string[] args)
         {
+            try
+            {
+                String str = Console.ReadLine();
+                Regex[] patterns = {new Regex(@"(^\d{3}-\d{3}$)"),
+                                    new Regex(@"(^\d{2}-\d{2}-\d{2}$)"),
+                                    new Regex(@"(^\d{3}-\d{2}-\d{2}$)")};
+                if (!string.IsNullOrEmpty(str))
+                {
+                    Console.WriteLine("Номера телефонов:");
+                    String[] str1 = str.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach(String item in str1)
+                    {
+                        foreach(Regex pat in patterns)
+                        {
+                            if (pat.IsMatch(item.Trim(new char[] { ',', '.', ';', '!', '?', '\"', '\'', ':' })))
+                            {
+                                Console.WriteLine(item.Trim(new char[] { ',', '.', ';', '!', '?', '\"', '\'', ':' }));
+                            }
+                        }
+                    }
+                    Console.ReadKey();
+                }
+                else
+                {
+                    throw new Exception("Строка пуста");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
         }
     }
 }
---
+++ Z08_1_WF/Z08_1_WF/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Z08_1_WF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            String str = richTextBoxText.Text;
+            Regex[] patterns = {new Regex(@"(^\d{3}-\d{3}$)"),
+                                    new Regex(@"(^\d{2}-\d{2}-\d{2}$)"),
+                                    new Regex(@"(^\d{3}-\d{2}-\d{2}$)")};
+            if (!string.IsNullOrEmpty(str))
+            {
+                richTextBoxAnswer.Text = "Номера телефонов:\n";
+                String[] str1 = str.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (String item in str1)
+                {
+                    foreach (Regex pat in patterns)
+                    {
+                        if (pat.IsMatch(item.Trim(new char[] { ',', '.', ';', '!', '?', '\"', '\'', ':' })))
+                        {
+                            richTextBoxAnswer.Text += item.Trim(new char[] { ',', '.', ';', '!', '?', '\"', '\'', ':' }) + "\n";
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show(text: "Строка пуста");
+            }
+        }
+    }
+}
